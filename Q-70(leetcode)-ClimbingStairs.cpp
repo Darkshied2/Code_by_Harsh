fe6936608f48ad65e0a1699bd791a34bd29f4b3d
Darkshied2 <112900012+Darkshied2@users.noreply.github.com>
2024-03-05 16:43:57 +0530

    Create Q-70(leetcode)-ClimbingStairs.cpp
    simple DP solution

---
+++ Q-70(leetcode)-ClimbingStairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int solve(int n, vector<int>&dp){
+    if (n<=2){
+        return n;
+    }
+    if (dp[n]!=-1){
+        return dp[n];
+    }
+    dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    return dp[n];
+}
+    int climbStairs(int n) {
+        if (n<=2)
+         return n;
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
