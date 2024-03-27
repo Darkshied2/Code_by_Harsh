377944e4baabbe4d3ce0efb2589f6f193f274504
Darkshied2 <112900012+Darkshied2@users.noreply.github.com>
2024-03-27 08:31:18 +0530

    Create Q-713(leetcode)Subarray Product Less Than K.cpp
    using sliding window

---
+++ Q-713(leetcode)Subarray Product Less Than K.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /* 
+    * Approach:
+    * Utilize a variable-sized sliding window approach to find the number of subarrays with a product less than a given threshold, k. Initially, both left and right pointers are set to the beginning of the array. We continuously shift the right pointer to the right, multiplying the product by the value at the new position. If the product exceeds k, we shrink the window by shifting the left pointer to the right until the product falls below k.
+    *
+    * 
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(1)
+    */
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) 
+    {
+        if(k <= 1)
+            return 0;
+        int count{}, left{}, product{1};
+        for(int i = 0 ;i < (int)nums.size() ;i++)
+        {
+            product *= nums[i];
+            while(product >= k)
+                product /= nums[left++];
+            
+            count +=i - left + 1;
+        }
+        
+        return count;
+    }
+};
